2b36868dd8efc91aa6b69c87456f0dcd977a5312
SrijitaGupta <srijita.gupta@iic.ac.in>
2025-05-27 14:12:46 +0530

    Initil commit

---
+++ hackathon-app/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';  // <-- Import your CSS file here
+
+export default function App() {
+  return (
+    <h1 className="my-heading">
+      hi
+    </h1>
+  );
+}
